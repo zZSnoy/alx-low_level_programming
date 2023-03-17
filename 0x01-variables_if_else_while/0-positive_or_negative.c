a3e231f52e166513886ff8ccb70639e4cbaa15bd
Youness <younessmarour7@gmail.com>
2023-03-17 15:41:56 -0700

    Betty for TW

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry Point
+ * Description: Calculating if positive, negative or zero
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
