f9cc34bc0bf904e57dd7087eaeffab1d51d3bd9b
Youness <younessmarour7@gmail.com>
2023-03-17 19:25:16 -0700

    Comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Printing single digit numbers.. again..
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
