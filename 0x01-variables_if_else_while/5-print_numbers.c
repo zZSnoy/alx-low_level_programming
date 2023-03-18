28406259ef4af819585e826b1efca22230fcfbc4
Youness <younessmarour7@gmail.com>
2023-03-17 18:40:37 -0700

    Single

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Single digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf('\n');
+	return (0);
+}
