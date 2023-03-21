184d992a81c1399edee4bb082b746b5baf36a16c
Youness <younessmarour7@gmail.com>
2023-03-21 14:30:08 -0700

    Alphatbeaaat

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Prints alphabet in lowercase
+ * Return: Always 0
+ */
+void print_alphabet(void);
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar("\n");
+}
