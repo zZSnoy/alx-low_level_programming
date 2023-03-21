0a6b75c9fe706bb5d83ffda903fccfeeeb82383a
Youness <younessmarour7@gmail.com>
2023-03-21 16:42:26 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - Print every minute of JB's Day
+ * Return: None
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 3; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a == 2 && b == 4)
+						break;
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(58);
+					_putchar(c + 48);
+					_putchar(d + 48);
+				}
+			}
+		}
+	}
+}
