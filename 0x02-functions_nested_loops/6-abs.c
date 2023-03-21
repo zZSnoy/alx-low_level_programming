3cfc616df84c3b422bd87bc71aafa1372974e582
Youness <younessmarour7@gmail.com>
2023-03-21 16:08:48 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computing absolute value of integer
+ * @n: Input number
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
