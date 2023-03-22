693c3b32015b2a451e4fc330e78206915a4a2e13
Youness <younessmarour7@gmail.com>
2023-03-21 18:54:01 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,11 +19,11 @@ void jack_bauer(void)
 					{
 						break;
 					}
-					_putchar(a + 48);
-					_putchar(b + 48);
+					_putchar(a + '0');
+					_putchar(b + '0');
 					_putchar(58);
-					_putchar(c + 48);
-					_putchar(d + 48);
+					_putchar(c + '0');
+					_putchar(d + '0');
 					_putchar('\n');
 				}
 			}
