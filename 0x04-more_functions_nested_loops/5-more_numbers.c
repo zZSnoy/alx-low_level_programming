9a10bd52cfb261ef8955fe396ed0afc823c2837b
Youness <younessmarour7@gmail.com>
2023-03-23 19:03:17 -0700

    More numbers x10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j == 14)
-				_putchar(j);
-			break;
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
 		}
 		_putchar('\n');
 	}
