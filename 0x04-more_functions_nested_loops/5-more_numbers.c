5c19a1dc38aff058385ec5533bd3641dbe7936c3
Youness <younessmarour7@gmail.com>
2023-03-23 18:49:26 -0700

    More numbers x10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j == 14)
+				_putchar(j);
+			break;
+		}
+		_putchar('\n');
+	}
+}
