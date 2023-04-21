db1f8858b83289c52f4c99b6118209f9272c71fe
Youness <younessmarour7@gmail.com>
2023-04-20 18:25:40 -0700

    All

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -32,7 +32,7 @@ void print_all(const char * const format, ...)
 					break;
 				case 's':
 					str = va_arg(list, char *);
-					if (!str)
+						if (!str)
 						str = "(nil)";
 						printf("%s%s", sep, str);
 						break;
